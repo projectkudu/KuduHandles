6e9d4b1c7996c453e4b210bf5f60d44d59a3c9ca
Ahmed ElSayed <ahmels@microsoft.com>
2013-10-11 19:05:13 -0700

    KuduHandles a tool to get all the file handle opened in a process from usermode

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace KuduHandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 1)
+            {
+                PrintUsage();
+                return;
+            }
+
+            try
+            {
+                foreach (var s in SystemUtility.GetHandles(Int32.Parse(args[0])).Where(handle => (handle.Type == HandleType.File)).ToList())
+                {
+                    if (s.DosFilePath != null)
+                        Console.WriteLine(s.DosFilePath);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("[Error] Can't get open file handles for process {0}", args[0]);
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("[Usage] KuduHandles.exe <ProcessId>");
+        }
+    }
+}
---
+++ SystemUtility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace KuduHandles
+{
+    class SystemUtility
+    {
+        public static IEnumerable<Handle> GetHandles(int processId)
+        {
+            uint length = 0x10000;
+            IntPtr ptr = IntPtr.Zero;
+            try
+            {
+                try { }
+                finally
+                {
+                    ptr = Marshal.AllocHGlobal((int)length);
+                }
+
+
+                uint returnLength;
+                NTSTATUS result;
+                while ((result = NativeMethods.NtQuerySystemInformation(
+                    SYSTEM_INFORMATION_CLASS.SystemHandleInformation, ptr, length, out returnLength)) ==
+                       NTSTATUS.STATUS_INFO_LENGTH_MISMATCH)
+                {
+                    length = ((returnLength + 0xffff) & ~(uint)0xffff);
+                    try { }
+                    finally
+                    {
+                        Marshal.FreeHGlobal(ptr);
+                        ptr = Marshal.AllocHGlobal((int)length);
+                    }
+                }
+
+                if (result != NTSTATUS.STATUS_SUCCESS)
+                    yield break;
+
+                int handleCount = IntPtr.Size == 4 ? Marshal.ReadInt32(ptr) : (int)Marshal.ReadInt64(ptr);
+                int offset = IntPtr.Size;
+                int size = Marshal.SizeOf(typeof(SYSTEM_HANDLE_ENTRY));
+                for (int i = 0; i < handleCount; i++)
+                {
+                    var handleEntry =
+                        (SYSTEM_HANDLE_ENTRY)Marshal.PtrToStructure(
+                        IntPtr.Add(ptr, offset), typeof(SYSTEM_HANDLE_ENTRY));
+
+                    if (handleEntry.OwnerProcessId == processId)
+                    {
+                        yield return new Handle(
+                            handleEntry.OwnerProcessId,
+                            handleEntry.Handle,
+                            handleEntry.ObjectTypeNumber);
+                    }
+
+                    offset += size;
+                }
+            }
+            finally
+            {
+                if (ptr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(ptr);
+            }
+        }
+    }
+}
